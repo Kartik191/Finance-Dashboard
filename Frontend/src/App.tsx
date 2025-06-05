2b9316da0079e56a8f5dbacb788f3f6d96767d65
ShreyasSharma0 <shreyas.college.study@gmail.com>
2025-06-05 18:15:23 +0530

    Added React Routes and Navbar

--- Frontend/src/App.tsx
+++ Frontend/src/App.tsx
@@ -1,7 +1,11 @@
 import { createTheme, ThemeProvider } from "@mui/material/styles";
 import { useMemo } from "react";
 import { themeSettings } from "./theme";
-import { CssBaseline } from "@mui/material";
+import { Box, CssBaseline } from "@mui/material";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Navbar from "./scenes/navbar";
+import Dashboard from "./scenes/dashboard";
+
 
 function App() {
   const theme = useMemo(() => createTheme(themeSettings), []);
@@ -9,9 +13,18 @@ function App() {
   return (
     <>
       <div className="app">
+        <BrowserRouter>
         <ThemeProvider theme={theme}>
           <CssBaseline />
+          <Box width="100%" height="100%" padding="1rem 2rem 4rem 2rem">
+          <Navbar />
+          <Routes>
+            <Route path="/" element= {<Dashboard />} />
+            <Route path="/predictions" element= {<div>predictions page</div>} />
+          </Routes>
+          </Box>
         </ThemeProvider>
+        </BrowserRouter>
       </div>
     </>
   );
