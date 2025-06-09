fa63730c4666eabb892e80422ff330a3f58d1f2d
hxnyeol <pvrohanraj@gmail.com>
2025-06-09 15:52:16 +0530

    add redux code & interface for /product and refactoring

--- Frontend/src/scenes/dashboard/Row2.tsx
+++ Frontend/src/scenes/dashboard/Row2.tsx
@@ -1,10 +1,15 @@
 
+import { useGetProductsQuery } from "@/state/api";
 import DashboardBox from "../../components/DashboardBox"
 
 type Props = {};
 const Row2 = (props: Props) => {
+    const { data } = useGetProductsQuery();
+    console.log("got data");
+    console.log(data);
     return(
         <>
+            <DashboardBox gridArea="d"></DashboardBox>
             {/* <DashboardBox gridArea="e"></DashboardBox> */}
             <DashboardBox gridArea="f"></DashboardBox>
             {/* <DashboardBox gridArea="g"></DashboardBox> */}
--- Frontend/src/state/api.ts
+++ Frontend/src/state/api.ts
@@ -1,18 +1,23 @@
 // docx referred https://redux-toolkit.js.org/rtk-query/api/createApi
 
 import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react';
+import { GetProductResponse } from './types';
 
 
 export const api = createApi({
-    baseQuery: fetchBaseQuery({baseUrl: import.meta.env.VITE_BASE_URL}), // Base URL + 
+    baseQuery: fetchBaseQuery({baseUrl: import.meta.env.VITE_SERVER_URL}), // Base URL + 
     reducerPath: "main",
-    tagTypes: ["Kpis"],
+    tagTypes: ["Kpis", "Products"],
     endpoints: (build) => ({
         getKpis: build.query<void, void>({ // Need to change Void, Void to other desired arguments
             query: () => "kpi/kpis/",  
             providesTags: ["Kpis"]
         }),
         // add more API Calls here
+        getProducts: build.query<Array<GetProductResponse>, void>({ // Need to change Void, Void to other desired arguments
+            query: () => "product/products/",  
+            providesTags: ["Products"]
+        }),
     })
 
 
@@ -20,5 +25,5 @@ export const api = createApi({
 
 
 //HOOK:  createApi: adds a suffix Query, Prefix use, first letter gets capitalized: in this case 'g' from getKpis
-export const { useGetKpisQuery } = api;
+export const { useGetKpisQuery, useGetProductsQuery } = api;
 
