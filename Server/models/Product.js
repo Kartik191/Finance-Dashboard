53c44fbb9246b475530ac58eca430c0d005fc31b
hxnyeol <pvrohanraj@gmail.com>
2025-06-09 12:37:39 +0530

    add product route and model

---
+++ Server/models/Product.js
@@ -0,0 +1,30 @@
+import mongoose from "mongoose";
+import { loadType } from "mongoose-currency";
+loadType(mongoose); 
+const Schema = mongoose.Schema;
+
+var currency = mongoose.Types.Currency;
+
+var ProductSchema = new Schema({
+    price: {
+        type: mongoose.Types.Currency,
+        currency: "USD",
+        get: (v) => v/100
+    },
+    expense: {
+        type: mongoose.Types.Currency,
+        currency: "USD",
+        get: (v) => v/100
+
+    }, 
+    transactions: [
+        {
+            type: mongoose.Schema.Types.ObjectId,
+            ref: "Transaction",
+        },
+    ],
+
+}, {timestamps: true, toJSON: {getters: true}})
+
+const Product = mongoose.model("Product", ProductSchema);
+export default Product;
